912406c2345275dc2cb4c466760963c140894a52
ercanakalar1995 <ercanakalar@gmail.com>
2024-02-22 00:13:43 +0300

    fix: :lock: clear profile cookie

--- backend/src/controllers/auth/signout.ts
+++ backend/src/controllers/auth/signout.ts
@@ -1,6 +1,7 @@
 const signOut = async (context: any) => {
   context.req.headers.cookie = null;
   context.res.clearCookie('user');
+  context.res.clearCookie('profile');
   return 'User signed out successfully!';
 };
 
