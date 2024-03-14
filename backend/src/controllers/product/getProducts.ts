230ebc4b18242969e298ff8f206210d70960e7a0
ercanakalar1995 <ercanakalar@gmail.com>
2024-03-14 21:08:25 +0300

    feat: :sparkles: get products

---
+++ backend/src/controllers/product/getProducts.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from 'express';
+import { Product } from '../../models/product-model/product-model';
+import { NotFoundError } from '../../errors';
+
+const getProducts = async (req: Request, res: Response) => {
+  const products = await Product.find({});
+
+  if (products.length === 0) {
+    throw new NotFoundError('Products not found');
+  }
+
+  const data = products.map((product) => {
+    return {
+      userId: product.userId,
+      category: product.category,
+      subCategory: product.subCategory,
+      group: product.group,
+      children: product.children,
+      name: product.name,
+      price: product.price,
+      description: product.description,
+      rating: product.rating,
+      stock: product.stock,
+      sold: product.sold,
+      images: product.images,
+      shipping: product.shipping,
+      brand: product.brand,
+    };
+  });
+
+  return {
+    message: 'Products found!',
+    data,
+  };
+};
+
+export { getProducts };
