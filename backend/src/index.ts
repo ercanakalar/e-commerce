e065930c8f3cba5755166972b426cbd2f14fa6a7
ercanakalar <ercanakalar@gmail.com>
2024-03-24 16:44:32 +0300

    refactor: :recycle: create table and change modal for product

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,4 @@
+import { create } from 'domain';
 import App from './app';
 import { Database } from './config/db';
 import { createAuthTable } from './models/auth-model/auth-model';
@@ -6,6 +7,7 @@ import { createGroupCategoryTable } from './models/category-model/category-group
 import { createCategoryTable } from './models/category-model/category-model';
 import { createSubCategoryTable } from './models/category-model/sub-category-model/sub-category-model';
 import { createProfileTable } from './models/profile-model/profile-model';
+import { createProductTable } from './models/product-model/product-model';
 
 const start = async () => {
   try {
@@ -16,6 +18,7 @@ const start = async () => {
     createSubCategoryTable()
     createGroupCategoryTable()
     createChildrenCategoryTable()
+    createProductTable()
     const app = new App();
     app.run();
   } catch (error) {
