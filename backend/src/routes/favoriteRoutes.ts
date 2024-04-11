6fe20a67d6240f0f5dd168668629fd5c3c4cc32e
ercanakalar <ercanakalar@gmail.com>
2024-04-11 16:11:56 +0300

    feat: :truck: favotire

--- backend/src/routes/favoriteRoutes.ts
+++ backend/src/routes/favoriteRoutes.ts
@@ -1,6 +1,12 @@
 import express from 'express';
+import favoriteResolvers from '../models/favorite-model/favorite-resolvers';
 
 const router = express.Router();
+
+router.use('add', favoriteResolvers.Mutation.addFavorite)
+router.use('remove', favoriteResolvers.Mutation.removeFavorite)
+
+router.use('get-all', favoriteResolvers.Query.getFavorites)
 
 
 export { router as favoriteRouter };
