c0a031c403dc27f07e5a2cc737ae05c9a77ee6e4
ercanakalar <ercanakalar@gmail.com>
2024-04-15 21:44:15 +0300

    refactor: :label: new type added for order

--- backend/src/types/order/order.interface.ts
+++ backend/src/types/order/order.interface.ts
@@ -1,9 +1,13 @@
 export interface IOrder {
-  authId: string;
   productId: string[];
   addressId: string;
   quantity: number[];
   totalPrice: number;
+}
+
+export interface IOrderUpdate {
+  orderId: string;
+  status: string;
 }
 
 export interface IOrderId {
@@ -11,14 +15,14 @@ export interface IOrderId {
 }
 
 export interface IOrderDatabase {
-    id: number;
-    auth_id: number;
-    product_id: number[];
-    address_id: number;
-    quantity: number[];
-    order_status: string;
-    total_price: number;
-    status: string;
-    created_at: Date;
-    updated_at: Date;
+  id: number;
+  auth_id: number;
+  product_id: number[];
+  address_id: number;
+  quantity: number[];
+  order_status: string;
+  total_price: number;
+  status: string;
+  created_at: Date;
+  updated_at: Date;
 }
