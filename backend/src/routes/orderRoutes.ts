dafd5e63b656015fec66d6fde7c717191dc56b38
ercanakalar <ercanakalar@gmail.com>
2024-04-15 21:43:49 +0300

    refactor: :truck: removed cancel and added update routes

--- backend/src/routes/orderRoutes.ts
+++ backend/src/routes/orderRoutes.ts
@@ -4,7 +4,7 @@ import orderResolvers from '../models/order-model/order-resolvers';
 const router = express.Router();
 
 router.use('create', orderResolvers.Mutation.createOrder)
-router.use('cancel', orderResolvers.Mutation.cancelOrder)
+router.use('update', orderResolvers.Mutation.updateOrder)
 router.use('get-by-id', orderResolvers.Mutation.getOrderById)
 
 router.use('get', orderResolvers.Query.getOrders)
