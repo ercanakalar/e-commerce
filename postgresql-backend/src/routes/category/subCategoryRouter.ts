722f0b4f3f7739a281b2c50f19283cdabb4fa2d3
ercanakalar <ercanakalar@gmail.com>
2024-05-17 19:17:28 +0300

    refactor: :fire: get rid of the routes

--- postgresql-backend/src/routes/category/subCategoryRouter.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import subCategoryResolvers from '../../models/category-model/sub-category-model/sub-category-resolvers';
-
-const router = express.Router();
-
-router.use('/', subCategoryResolvers.Query.getSubCategories);
-router.use('/create', subCategoryResolvers.Mutation.createSubCategory);
-
-export { router as subCategoryRouter };
--- postgresql-backend/src/routes/profileRouter.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import profileResolvers from '../models/profile-model/profile-resolvers';
-
-const router = express.Router();
-
-router.use('/get-own-profile', profileResolvers.Query.getOwnProfile);
-router.use('/update-profile', profileResolvers.Mutation.updateProfile);
-
-export { router as profileRouter };
