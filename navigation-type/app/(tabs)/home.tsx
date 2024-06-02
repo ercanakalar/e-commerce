63698952b8b6216bebf7621310627060cac9a302
ercanakalar <ercanakalar@gmail.com>
2024-06-02 16:42:39 +0300

    chore: :truck: changed project's name

--- navigation-type/app/(tabs)/home.tsx
+++ navigation-type/app/(tabs)/home.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
 
 import { Text, View } from '@/components/Themed';
+import SearchBar from '@/components/shared/SearchBar';
 
 export default function TabOneScreen() {
+  const [searchQuery, setSearchQuery] = useState<string>('');
+
   return (
     <View style={styles.container}>
+      <SearchBar value={searchQuery} onChangeText={setSearchQuery} />
       <Text style={styles.title}>Home Screen will be added</Text>
     </View>
   );
