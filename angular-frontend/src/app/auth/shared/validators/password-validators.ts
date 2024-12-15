006c76a8017372a60a3b0ebc7966745d08ccbab8
ercanakalar <ercanakalar@gmail.com>
2024-12-15 17:16:07 +0300

    feat: :safety_vest: Frontend-Angular: add validation to show error of the password match

---
+++ angular-frontend/src/app/auth/shared/validators/password-validators.ts
@@ -0,0 +1,14 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+export function passwordMatchValidator(): ValidatorFn {
+  return (group: AbstractControl): ValidationErrors | null => {
+    const password = group.get('password')?.value;
+    const confirmPassword = group.get('confirmPassword')?.value;
+
+    if (password && confirmPassword && password !== confirmPassword) {
+      return { passwordMismatch: true };
+    }
+
+    return null;
+  };
+}
