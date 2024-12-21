cf3edc4ebe674dd269783a56876b9f6ea5d95257
ercanakalar <ercanakalar@gmail.com>
2024-12-21 11:24:58 +0300

    feat: :sparkles: Frontend-Angular: continue signup process

--- angular-frontend/src/app/auth/shared/service/auth.service.ts
+++ angular-frontend/src/app/auth/shared/service/auth.service.ts
@@ -1,32 +1,51 @@
 import { Injectable } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
+
+import { JwtService } from '../../../shared/service/jwt.service';
 import { ApiService } from '../../../shared/service/api.service';
 
+import {
+  SignInRequest,
+  SignInResponse,
+  SignUpRequest,
+} from '../types/user.type';
+
+import { signInMutation, signUpMutation } from '../graphql/auth.mutation';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(
+    private api: ApiService,
+    private jwtService: JwtService,
+  ) {}
 
-  constructor(private api: ApiService) { }
-
-  signIn(variables: { email: string, password: string }) {
-    const graphqlQuery = `
-      mutation SignInMutation($email: String!, $password: String!) {
-        signIn(email: $email, password: $password) {
-          message
-          data {
-            id
-            firstName
-            lastName
-            email
-          }
-          token
-        }
-      }
-    `;
+  signIn(variables: SignInRequest) {
     this.api
-      .post('http://localhost:4000/api/v1/auth/signin', { query: graphqlQuery, variables })
-      .subscribe((res) => {
+      .post('http://localhost:4000', {
+        query: signInMutation,
+        variables,
+      })
+      .subscribe((res: HttpResponse<any>) => {
+        const token = (res.body.data as SignInResponse).token;
+        this.jwtService.setToken(token);
+        this.jwtService.handleTokenNavigation();
+      });
+  }
+
+  signUp(variables: SignUpRequest) {
+    this.api
+      .post('http://localhost:4000', {
+        query: signUpMutation,
+        variables,
+      })
+      .subscribe((res: HttpResponse<any>) => {
         console.log(res);
+        
+        const token = (res.body.data as SignInResponse).token;
+        this.jwtService.setToken(token);
+        this.jwtService.handleTokenNavigation();
       });
   }
 }
