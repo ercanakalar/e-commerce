83b265358085837a0a945a7b34b7f482b0d4e626
ercanakalar <ercanakalar@gmail.com>
2024-12-21 11:24:28 +0300

    feat: :sparkles: Frontend-Angular: Guard check

--- angular-frontend/src/app/auth/aut.routes.ts
+++ angular-frontend/src/app/auth/aut.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { authGuard } from './shared/guards/auth.guard';
 
 export const authRoutes: Routes = [
   {
     path: 'login',
+
     component: SigninComponent,
   },
   {
@@ -14,6 +16,7 @@ export const authRoutes: Routes = [
   },
   {
     path: 'forgot-password',
+    canActivate: [authGuard],
     component: ForgotPasswordComponent,
   },
 ];
