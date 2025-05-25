d34a263a8d9eacc856a762c0631f7fc84f06ecaa
ercanakalar <ercanakalar@gmail.com>
2025-05-25 23:39:23 +0300

    refactor: :fire: remove unused react

--- react-frontend/src/store/middlewares/createApi.ts
+++
@@ -1,9 +0,0 @@
-import { buildCreateApi, coreModule } from '@reduxjs/toolkit/query';
-import { reactHooksModule } from '@reduxjs/toolkit/query/react';
-
-const createApi = buildCreateApi(
-  coreModule(),
-  reactHooksModule({ unstable__sideEffectsInRender: false })
-);
-
-export default createApi;
--- react-frontend/src/store/reducer.ts
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import authenticationService from './services/authenticationService';
-
-const rootReducers = {
-  [authenticationService.reducerPath]: authenticationService.reducer,
-};
-export default combineReducers(rootReducers);
