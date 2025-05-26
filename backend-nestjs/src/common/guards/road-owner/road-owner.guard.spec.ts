a66022977f85c17645bab854299f7c01deb61822
ercanakalar <ercanakalar@gmail.com>
2025-05-26 19:49:29 +0300

    chore(Backend-NestJS:): :fire: remove road guard

--- backend-nestjs/src/common/guards/road-owner/road-owner.guard.spec.ts
+++
@@ -1,7 +0,0 @@
-import { RoadOwnerGuard } from './road-owner.guard';
-
-describe('RoadOwnerGuard', () => {
-  it('should be defined', () => {
-    expect(new RoadOwnerGuard()).toBeDefined();
-  });
-});
