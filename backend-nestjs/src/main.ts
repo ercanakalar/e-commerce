6a0b985b28ee82cce32dbd766f4c77e4f87ff5aa
ercanakalar <ercanakalar@gmail.com>
2025-05-27 07:37:38 +0300

    feat(Backend-NestJS:): :sparkles: update error handler as global

--- backend-nestjs/src/main.ts
+++ backend-nestjs/src/main.ts
@@ -1,18 +1,20 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
+import { PrismaClientExceptionFilter } from 'prisma/prisma-client-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Enable CORS
   app.enableCors({
-    origin: 'http://localhost:4200', // Allow requests from Angular frontend
-    credentials: true, // Allow cookies if needed
+    origin: 'http://localhost:4200',
+    credentials: true,
   });
 
   app.useGlobalPipes(new ValidationPipe());
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter))
   await app.listen(3000);
 }
 bootstrap();
